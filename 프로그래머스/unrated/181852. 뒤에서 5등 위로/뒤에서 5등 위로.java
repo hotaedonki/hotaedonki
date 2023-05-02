3bc00a5cd2d3fb312be7da74c07ddb6f1ff02ded
hotaedonki <132128508+hotaedonki@users.noreply.github.com>
2023-05-02 20:39:38 +0900

    [unrated] Title: 뒤에서 5등 위로, Time: 0.47 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
+++ 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -1,14 +1,7 @@
 import java.util.*;
 class Solution {
-    public ArrayList<Integer> solution(int[] num_list) {
+    public int[] solution(int[] num_list) {
         Arrays.sort(num_list);
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        for(int i = 0; i < num_list.length; i++){
-            int a = num_list[i];
-            list.add(a);   
-        }
-        for(int i = 0; i < 5; i++)
-            list.remove(0);
-        return list;
+        return Arrays.copyOfRange(num_list, 5, num_list.length);
     }
 }
