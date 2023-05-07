7f857025048942a66146423bea0c2afb27edb8f5
hotaedonki <132128508+hotaedonki@users.noreply.github.com>
2023-05-07 16:19:06 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 1.72 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,19 +1,9 @@
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
-        String[] arr = my_string.split("");
-        String[] arr2 = overwrite_string.split("");
+        String answer = my_string.substring(0, s) + overwrite_string;
         
-        String result = "";
-        
-        for(int i = 0; i < s; i++){
-            result += arr[i];
-        }
-        for(int i = 0; i < overwrite_string.length(); i++){
-            result += arr2[i];
-        }
-        for(int i = s + overwrite_string.length(); i < my_string.length(); i++){
-            result += arr[i];
-        }
-        return result;
+        if(answer.length() < my_string.length()){
+            return answer += my_string.substring(answer.length());
+        }return answer;
     }
 }
