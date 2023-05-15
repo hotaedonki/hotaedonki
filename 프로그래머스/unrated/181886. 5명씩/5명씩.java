767c75823a4f1d65bb3e31acc8a4e84d1e8167ad
hotaedonki <132128508+hotaedonki@users.noreply.github.com>
2023-05-15 19:10:32 +0900

    [unrated] Title: 5명씩, Time: 0.02 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Solution {
+    public ArrayList<String> solution(String[] names) {
+        ArrayList<String> list = new ArrayList<String>();
+        for(int i = 0; i<names.length; i++){
+            if(i % 5 == 0)
+                list.add(names[i]);
+        }return list;
+    }
+}
