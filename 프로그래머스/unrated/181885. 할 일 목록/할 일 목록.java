d637ea2b9bd581aba85fd1a9c92cdc07d6020d27
hotaedonki <132128508+hotaedonki@users.noreply.github.com>
2023-05-15 19:47:59 +0900

    [unrated] Title: 할 일 목록, Time: 0.03 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList<String> solution(String[] todo_list, boolean[] finished) {
+        ArrayList<String> list = new ArrayList<String>();
+        for(int i = 0; i < todo_list.length; i++){
+            if(!finished[i])
+                list.add(todo_list[i]);
+            else continue;
+        }return list;
+    }
+}
