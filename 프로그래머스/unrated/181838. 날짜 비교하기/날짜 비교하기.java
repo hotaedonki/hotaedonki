ae917b7f8cd93d89c9fcefb5fa482981833ce2c0
hotaedonki <132128508+hotaedonki@users.noreply.github.com>
2023-05-17 20:15:32 +0900

    [unrated] Title: 날짜 비교하기, Time: 0.71 ms, Memory: 85.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.java
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.java
@@ -1,24 +1,7 @@
+import java.time.LocalDate;
 class Solution {
     public int solution(int[] date1, int[] date2) {
-        int answer = 1;
-        if(date1[0] < date2[0]) {
-	    		return answer;
-	    	}
-	    else if(date1[0] == date2[0]) {
-	    	if(date1[1] < date2[1]){
-	    		return answer;
-            }
-	    	else if(date1[1] == date2[1]) {
-	    		if(date1[2] < date2[2])
-	    			return answer;
-	    	    else return answer-1;
-	    	}
-            else {
-                return answer-1;
-            }
-	    }
-	    else {
-            return answer-1;
-        }
+    return LocalDate.of(date1[0],date1[1],date1[2])
+        .isBefore(LocalDate.of(date2[0],date2[1],date2[2])) ? 1:0;
     }
 }
