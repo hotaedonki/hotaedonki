290745344508d42a132260c6b8eb9415ce7498d1
hotaedonki <132128508+hotaedonki@users.noreply.github.com>
2023-05-17 20:09:04 +0900

    [unrated] Title: 날짜 비교하기, Time: 0.02 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int[] date1, int[] date2) {
+        int answer = 1;
+        if(date1[0] < date2[0]) {
+	    		return answer;
+	    	}
+	    else if(date1[0] == date2[0]) {
+	    	if(date1[1] < date2[1]){
+	    		return answer;
+            }
+	    	else if(date1[1] == date2[1]) {
+	    		if(date1[2] < date2[2])
+	    			return answer;
+	    	    else return answer-1;
+	    	}
+            else {
+                return answer-1;
+            }
+	    }
+	    else {
+            return answer-1;
+        }
+    }
+}
