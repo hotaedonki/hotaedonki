d31d12f65554c95a6721815e1902dde9afb5ddbd
hotaedonki <132128508+hotaedonki@users.noreply.github.com>
2023-05-26 18:35:50 +0900

    [level 0] Title: A로 B 만들기, Time: 0.58 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Solution {
+    public int solution(String before, String after) {
+		char[] b = before.toCharArray();
+		char[] a = after.toCharArray();	
+		Arrays.sort(b);
+		Arrays.sort(a);
+		
+        return new String(a).equals(new String(b)) ? 1 : 0;
+    }
+}
