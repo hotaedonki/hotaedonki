0b6631a3f7d89a2ef3bb4172933a8b28158cf2cf
hotaedonki <132128508+hotaedonki@users.noreply.github.com>
2023-09-29 14:31:59 +0900

    [Bronze V] Title: 윤년, Time: 128 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		String s = bf.readLine();
+		
+		int year = Integer.parseInt(s);
+		if(year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) System.out.print(1);
+		else System.out.print(0);
+	}
+}
