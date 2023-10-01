4a13e424c3f338cbdf68c8499d80d07fc4c34381
hotaedonki <132128508+hotaedonki@users.noreply.github.com>
2023-10-01 17:18:08 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.30 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -1,53 +1,45 @@
-import java.util.ArrayList;
-import java.util.List;
-
-class Solution {
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+class Solution { 
     public int solution(String dartResult) {
-        int answer = 0;
-		int prevScore = 0;
-		int score = 0;
-		char bonus;
+        Pattern pattern = Pattern.compile("(\\d+)([SDT])([*#]?)");
+        Matcher matcher = pattern.matcher(dartResult);
 
-		for (int i = 0; i < dartResult.length(); i++) {
-			char c = dartResult.charAt(i);
-			
-			if (Character.isDigit(c)) {
-				score = c - '0';	
+        int[] scores = new int[3];
+        int idx = 0;
 
-				if (i < dartResult.length() - 1 && dartResult.charAt(i + 1) == '0') {
-					score = 10;
-					i++;
-				}
-			}
-			
-			else if (c == 'S' || c == 'D' || c == 'T') {
-				bonus = c;
-				switch (bonus) {
-					case 'D':
-						score = (int) Math.pow(score, 2);
-						break;
-					case 'T':
-						score = (int) Math.pow(score, 3);
-						break;
-				}
-			}
-            
-			else if (c == '*' || c == '#') {
-				if (c == '*') {
-					score *= 2;
-					answer += prevScore; 
-				} else {
-					score *= -1;
-				}
-			}
+        while (matcher.find()) {
+            int score = Integer.parseInt(matcher.group(1));
+            char bonus = matcher.group(2).charAt(0);
+            char option = matcher.group(3).isEmpty() ? ' ' : matcher.group(3).charAt(0);
 
-			if (i == dartResult.length() - 1 || Character.isDigit(dartResult.charAt(i + 1))) {
-				answer += score;
-				prevScore = score;
-				score = 0; 
-			}
-		}
-		
-		return answer;
+            switch (bonus) {
+                case 'S':
+                    scores[idx] = score;
+                    break;
+                case 'D':
+                    scores[idx] = (int) Math.pow(score, 2);
+                    break;
+                case 'T':
+                    scores[idx] = (int) Math.pow(score, 3);
+                    break;
+            }
+
+            if (option == '*') {
+                if (idx > 0) scores[idx - 1] *= 2;
+                scores[idx] *= 2;
+            } else if (option == '#') {
+                scores[idx] = -scores[idx];
+            }
+
+            idx++;
+        }
+
+        int total = 0;
+        for (int i = 0; i < 3; i++) {
+            total += scores[i];
+        }
+
+        return total;
     }
 }
