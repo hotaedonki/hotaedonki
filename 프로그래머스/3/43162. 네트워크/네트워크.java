8d2428d61aec0d9c885dd8f58c1231c453cd1e0d
Kim Taeho <132128508+hotaedonki@users.noreply.github.com>
2023-10-13 17:00:26 +0900

    [level 3] Title: 네트워크, Time: 0.09 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int n, int[][] computers) {
+        boolean[] visited = new boolean[n];
+        int count = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(i, visited, computers);
+                count++;
+            }
+        }
+        
+        return count;
+    }
+    
+    public void dfs(int node, boolean[] visited, int[][] computers) {
+        visited[node] = true;
+
+        for (int i = 0; i < computers.length; i++) {
+            if (computers[node][i] == 1 && !visited[i]) {
+                dfs(i, visited, computers);
+            }
+        }
+    }
+}
