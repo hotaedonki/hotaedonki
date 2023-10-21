de23803f81e784ac45dbc32455baf4839e4ad590
Kim Taeho <132128508+hotaedonki@users.noreply.github.com>
2023-10-21 13:14:53 +0900

    [level 2] Title: 주식가격, Time: 11.09 ms, Memory: 60.9 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,28 +2,16 @@ import java.util.Stack;
 
 class Solution {
     public int[] solution(int[] prices) {
-        //결과를 담을 배열
-        int[] answer = new int [prices.length];
-        
-        Stack<Integer> stack = new Stack<>();
-        
-        for(int i = 0; i < prices.length; i++) {
-           //1. 가격이 떨어진 경우에 떨어진 데까지 걸린 시간 입력
-            while(!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
-                int index = stack.pop();
-                answer[index] = i - index;
+        int len = prices.length;
+        int[] answer = new int[len];
+        int i, j;
+        for (i = 0; i < len; i++) {
+            for (j = i + 1; j < len; j++) {
+                answer[i]++;
+                if (prices[i] > prices[j])
+                    break;
             }
-            
-            //2. stack에 매초의 시점에 해당하는 index추가
-            stack.push(i);
         }
-        
-        //가격이 떨어지지 않은 요소들의 시간을 계산
-        while(!stack.isEmpty()) {
-            int index = stack.pop();
-            answer[index] = prices.length - 1 - index;
-        }
-        
         return answer;
     }
 }
